16eb707cd2d5d2a3beb84fd45a624e6b6487a9ff
DESKTOP-RESMSON\Colea <nicolaelungu8@gmail.com>
2018-01-19 23:23:05 +0200

    entity for StatusHistory

---
+++ ShopOnline/src/main/java/com/iquest/ShopOnline/Model/StatusHistory.java
@@ -0,0 +1,61 @@
+package com.iquest.ShopOnline.Model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Status_History")
+public class StatusHistory implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id", nullable = false)
+    private Order order;
+
+    @Id
+    @OneToOne
+    @JoinColumn(name = "status_id", referencedColumnName = "id", nullable = false)
+    private OrderStatus orderStatus;
+
+    @Column(name = "order_date", nullable = false)
+    private Date orderDate;
+
+    public StatusHistory() {
+
+    }
+
+    public StatusHistory(Order order, OrderStatus orderStatus, Date orderDate) {
+        this.order = order;
+        this.orderStatus = orderStatus;
+        this.orderDate = orderDate;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+}
---
+++ ShopOnline/src/main/java/com/iquest/ShopOnline/Model/StatusHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.iquest.ShopOnline.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StatusHistoryRepository extends CrudRepository<StatusHistory, Integer> {
+}
