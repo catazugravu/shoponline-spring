680af8375af971092b29f60476f40f67d837c027
DESKTOP-D9QM9N5\dursu <danielursuu@gmail.com>
2018-03-06 01:37:41 +0200

    Deleted unnecessary files

--- ShopOnline/src/main/java/com/iquest/ShopOnline/Model/ProductsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.iquest.ShopOnline.Model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductsRepository extends CrudRepository<Products, Integer> {
-}
