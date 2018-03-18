31ea9366cd653d752a870da014725717d4c9ad60
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-18 15:07:10 +0200

    Implement CartItemDto

---
+++ ShopOnline/src/main/java/com/iquest/shoponline/dto/cartItem/CartItemDto.java
@@ -0,0 +1,41 @@
+package com.iquest.shoponline.dto.cartItem;
+
+public class CartItemDto {
+    public Integer id;
+    public String name;
+    public Double price;
+    public Integer quantity;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
