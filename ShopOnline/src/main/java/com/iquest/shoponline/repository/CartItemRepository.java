3f52c802fb65632864f0d0bc8d7f598d01af4b77
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-18 15:05:23 +0200

    Implement methods for CartItemRepository
    Implement:
    *findAllByCart_Id
    *findByCartIdAndProductId
    *findByIdAndCartId
    *deleteByIdAndCartId

--- ShopOnline/src/main/java/com/iquest/shoponline/repository/CartItemRepository.java
+++ ShopOnline/src/main/java/com/iquest/shoponline/repository/CartItemRepository.java
@@ -1,7 +1,20 @@
 package com.iquest.shoponline.repository;
 
+import com.iquest.shoponline.dto.cartItem.CartItemDto;
 import com.iquest.shoponline.model.CartItem;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface CartItemRepository extends CrudRepository<CartItem, Integer> {
+
+    List<CartItem> findAllByCart_Id(Integer cartId);
+
+    CartItem findByCartIdAndProductId(Integer cartId, Integer productId);
+
+    CartItem findByIdAndCartId(Integer id, Integer cartId);
+
+    @Transactional
+    Integer deleteByIdAndCartId(Integer id, Integer cartId);
 }
