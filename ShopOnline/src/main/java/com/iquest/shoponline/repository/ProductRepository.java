477390053f44f9a1bf4bc470a1890b22b03dc0e7
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-18 15:06:57 +0200

    Implement methods for ProductRepository
    Implements:
    *findNameById
    *findPriceById

--- ShopOnline/src/main/java/com/iquest/shoponline/repository/ProductRepository.java
+++ ShopOnline/src/main/java/com/iquest/shoponline/repository/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.iquest.shoponline.repository;
 
 import com.iquest.shoponline.model.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    @Query("SELECT p.name FROM Product p where p.id = :id")
+    String findNameById(@Param("id") Integer id);
+
+    @Query("SELECT p.price FROM Product p where p.id = :id")
+    Double findPriceById(@Param("id") Integer id);
 }
