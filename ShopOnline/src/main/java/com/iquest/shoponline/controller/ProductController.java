1399da831d7602900b5dee8a36eee236edc1227d
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-21 11:15:39 +0200

    Create Product controller and service
    *Implement get all products

--- ShopOnline/src/main/java/com/iquest/shoponline/controller/ProductController.java
+++ ShopOnline/src/main/java/com/iquest/shoponline/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.iquest.shoponline.controller;
 
+import com.iquest.shoponline.constants.Views;
 import com.iquest.shoponline.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/product")
@@ -11,6 +15,12 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+
+    @GetMapping("/")
+    public String getProductList(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return Views.PRODUCT_PAGE;
+    }
 
 
 }
--- ShopOnline/src/main/java/com/iquest/shoponline/services/ProductService.java
+++ ShopOnline/src/main/java/com/iquest/shoponline/services/ProductService.java
@@ -1,12 +1,37 @@
 package com.iquest.shoponline.services;
 
+import com.iquest.shoponline.dto.product.ProductDtoList;
+import com.iquest.shoponline.model.Product;
 import com.iquest.shoponline.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
+
+    public List<ProductDtoList> findAll() {
+        Iterable<Product> products = productRepository.findAll();
+        List<ProductDtoList> dtos = new ArrayList<>();
+        products.forEach(product -> populateDto(dtos, product));
+        return dtos;
+    }
+
+    private void populateDto(List<ProductDtoList> dtos, Product product) {
+        ProductDtoList dto = new ProductDtoList();
+        dto.setId(product.getId());
+        dto.setCategoryId(product.getCategory().getName());
+        dto.setName(product.getName());
+        dto.setPrice(product.getPrice());
+        dto.setStock(product.getStock());
+        dto.setImgPath1(product.getImgPath1());
+        dto.setImgPath2(product.getImgPath2());
+
+        dtos.add(dto);
+    }
 }
